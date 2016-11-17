89d975ad9a1b6d42555de949beaf2c5269fc2816
Alan Ableson <ableson@queensu.ca>
2016-11-17 08:56:11 -0500

    Automatic update from (SurfaceAbleson)

--- PracticeProblems/MATLAB/W09Pendulum2.m
+++ PracticeProblems/MATLAB/W09Pendulum2.m
@@ -3,6 +3,7 @@ close all;
 L = 1.5; 
 theta0 = 7/8*pi;  % initial angle
 mu = 0.2; 
+g = 9.8;
 
 % Simulate the motion using ode45, and plot the angle graph, w(:,1)
 w0 = [theta0, 0];  % initial angle, and zero initial velocity
@@ -43,7 +44,7 @@ w0 = [theta0, v0];  % initial angle, and now an initial velocity
 tspan = [0, 10];  % simulate for one full period
 DE = @(t, w) pendulumDE(t, w, g, L, mu);  % last zero = friction coefficient.
 [t, w] = ode45(DE, tspan, w0);
-plot(t, w(:,1), 'linewidth', 2);of 
+plot(t, w(:,1), 'linewidth', 2);
 hold on;
 % re-run with a little smaller initial velocity
 w0 = [theta0, v0+0.1];  % initial angle, and now an initial velocity
